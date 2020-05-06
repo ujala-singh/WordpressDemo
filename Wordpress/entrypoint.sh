a1f9cef48a4b5b4aa68453caf016acc33baebf18
Ujala Singh <59909223+ujala-singh@users.noreply.github.com>
2020-05-06 13:34:23 +0530

    Delete entrypoint.sh

--- Wordpress/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sed -i "s/database_name_here/$WORDPRESS_DB_NAME/" /var/www/html/wp-config.php
-sed -i "s/username_here/$WORDPRESS_DB_USER/" /var/www/html/wp-config.php
-sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/" /var/www/html/wp-config.php
-sed -i "s/localhost/$WORDPRESS_DB_HOST/" /var/www/html/wp-config.php
-sed -i "s/false/true/" /var/www/html/wp-config.php
-
-/usr/sbin/apachectl -D FOREGROUND
